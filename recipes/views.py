59c6c0cc2a7956e13fb292436a9fe0d03eb4786f
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-04-02 18:18:09 -0300

    adicionado templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contato(request):
+    return HttpResponse('Contato1')
+
+
+def sobre(request):
+    return HttpResponse('sobre1')
