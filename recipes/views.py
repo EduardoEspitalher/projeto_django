5b65f9672f248551c2b4619aa9dd067322ecb6e2
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-04-03 17:13:29 -0300

    inicio da home

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Eduardo Espitalher Dias'
+    })
