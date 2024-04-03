af77d16116e988732651acd9f007e9f444b91d8d
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-04-03 16:41:04 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html', context={
-        'name': 'Eduardo Espitalher Dias',
-    })
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
